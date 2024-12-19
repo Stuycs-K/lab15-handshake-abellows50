610dc003d4d12c3679128c1fdd29e8a031b8f005
abellows50 <abellows50@stuy.edu>
2024-12-18 22:44:57 -0500

    some working

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,4 +1,7 @@
 #include "pipe_networking.h"
+
+#define MAX_PP_LEN 32
+
 //UPSTREAM = to the server / from the client
 //DOWNSTREAM = to the client / from the server
 /*=========================
@@ -10,7 +13,15 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_setup() {
-  int from_client = 0;
+  mkfifo("WKP", 0666);
+  //open WKP and block
+  printf("created WKP...\n");
+  int from_client = open("WKP",O_RDONLY, 0);
+  printf("WKP has opened...\n");
+
+  remove("WKP");
+  printf("WKP file removed...\n");
+
   return from_client;
 }
 
@@ -24,7 +35,39 @@ int server_setup() {
   returns the file descriptor for the upstream pipe (see server setup).
   =========================*/
 int server_handshake(int *to_client) {
-  int from_client;
+  /*============================
+  HANDSHAKE: Recieve SYN (PP name)
+             Send back SYN-ACK (pid)
+             recieve and check ACK (pid - 1)?
+             ==================================*/
+
+  int from_client = server_setup();
+  printf("obtained server link...\n");
+
+  //read the name of the PP -- this is the SYN
+  char pp_name[MAX_PP_LEN];
+  read(from_client, pp_name, MAX_PP_LEN);
+  printf("read pp_id name...\n");
+
+  //open the pp_name and set to_client
+  int to_client_real = open(pp_name, O_WRONLY, 0);
+  *to_client = to_client_real;
+  printf("oppened the pp_name channel...\n");
+
+  //send back SYN-ACK
+  int syn_ack = getpid();
+  write(to_client_real, &syn_ack, sizeof(int));
+
+  //recieve and confirm that ACK matches pid-1;
+  int ack;
+  read(from_client, &ack, sizeof(int));
+
+  if (ack != syn_ack - 1){
+    perror("breaking connection attempt - handshake failed");
+    exit(1); //err on response!
+  }
+  
+  //Handshake complete
   return from_client;
 }
 
@@ -39,7 +82,26 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  int from_server;
+  *to_server = open("WKP", O_WRONLY, 0);
+  if(*to_server == -1){
+    perror("err: server has not yet created WKP...");
+    exit(1);
+  }
+
+  char pp_name[MAX_PP_LEN];
+  sprintf(pp_name, "%d", getpid());
+  mkfifo(pp_name, 0666);
+
+  int from_server = open(pp_name, O_RDONLY, 0);
+
+  write(*to_server, pp_name, MAX_PP_LEN);
+
+  int syn_ack;
+  read(from_server, &syn_ack, sizeof(int));
+
+  int ack = syn_ack - 1;
+  write(*to_server, &ack, sizeof(int));
+
   return from_server;
 }
 
